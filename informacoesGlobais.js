6f4d2632bffdcf698684eedf3312d0bd411089da
raianahanna17 <160878224+raianahanna17@users.noreply.github.com>
2024-10-07 11:44:52 -0300

    Update informacoesGlobais.js

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -1,12 +1,11 @@
-const url = ‘https://raw.githubusercontent.com/
-guilhermeonrails/api/main/dados-globais.json’
+const url = ‘https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json’
+
 async function visualizarInformacoesGlobais() {
 const res = await fetch(url)
 const dados = await res.json()
-  document.createElement(‘p’)
-  paragrafo.classList.add(‘graficos-container__texto’)
-  const paragrafo=console.log(dados.tempo_medio);
-}
-visualizarInformacoesGlobais()
-
+const pessoasConectadas = (dados.total._pessoas_conectadas / 1e9)
+const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+const horas = parseInt(dados.temp_medio)
+const minutos = Math.round ((dados.tempo_medio- horas) * 100)
+const porcentagemConectada = ((
 
