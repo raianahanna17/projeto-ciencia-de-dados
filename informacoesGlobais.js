99110cc84dbe71b691ad893513e738e2d20bdccd
raianahanna17 <160878224+raianahanna17@users.noreply.github.com>
2024-10-12 14:59:53 -0300

    Update informacoesGlobais.js

---
+++ informacoesGlobais.js
@@ -0,0 +1,17 @@
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados.json'
+
+async function vizualizarInformacoesGlobais() {
+  const res = await
+
+
+
+
+
+
+
+
+
+
+
+
+paragrafo.innerHTML = “Você sabia que o mundo tem<span>${pessoasNoMundo} bilhões</span> de pessoas e queaproximadamente <span>${pessoasConectadas} bilhões</span> estãoconectadas em alguma rede social e passam em média <span>${horas}horas</span> e <span>${minutos} minutos</span> conectadas.<br>Issosignifica que aproximadamente<span>${porcentagemConectada}%</span> de pessoas estão conectadasem alguma rede social.”
