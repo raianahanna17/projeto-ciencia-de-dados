c03f9baac312c69a4cc4d79898cd5bae35eeefaa
raianahanna17 <160878224+raianahanna17@users.noreply.github.com>
2024-10-17 22:23:49 -0300

    Update commom.js

--- commom.js
+++ commom.js
@@ -1,12 +1,12 @@
 const getCSS = (variavel) => {
-    const bodyStyles = getComputedStyle(document.body)
-    return bodyStyles.getPropertyValue(variavel)
-}
-
-const tickConfig = {
-    family: getCSS('--font'),
-    size: 16,
-    color: getCSS('--primary-color')
-}
-
-export {getCSS, tickConfig}
+    return getComputedStyle(document.body).
+    getPropertyValue(variavel)
+    }
+    export {getCSS}
+    const tickConfig = {
+        color: getCSS(--primary-color),
+        size: 16,
+        family: getCSS(--font)
+        }
+        
+        export {getCSS, tickConfig}
